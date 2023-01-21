ba7db386590dd4bce2261f05460365f62200d10d
Jenya Shvartsman <jenyashvartsman@gmail.com>
2023-01-20 22:01:08 -0500

    basic layout

--- apps/client/src/app/layout/header/header.component.ts
+++ apps/client/src/app/layout/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'nx-kanban-board-header',
   standalone: true,
-  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
+  imports: [CommonModule, MatToolbarModule],
 })
 export class HeaderComponent {}
